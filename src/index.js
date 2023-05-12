d308c3ff4e90edb03af4bc1569950f8ec27a44b6
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-05-13 01:28:04 +0300

    Features section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { createRoot } from 'react-dom';
+
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// createRoot(document.getElementById('root')).render(<App />);
